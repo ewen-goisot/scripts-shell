93e260a502917576f27add0be419d254b75bdd2b
ewen-goisot <38837519+ewen-goisot@users.noreply.github.com>
2018-06-19 17:19:39 +0200

    Update youtube-dl-audio.sh

--- youtube-dl-audio.sh
+++ youtube-dl-audio.sh
@@ -9,8 +9,43 @@
 dossier=$(pwd)
 nom="url"
 
-# -o specifie l'endroit ou l'on envoie les audios, renvoie une erreur si le dossier n'existe pas
-if  [[ $1 = "-o" ]]; then
+# -h affiche l'aide puis quitte
+if [[ $1 = "-h" ]]; then
+	echo "
+
+Aide:
+
+Permet d'enregistrer les audios (musiques, audiobooks, ...) de vidéos youtube grâce à youtube-dl sans jamais télécharger la partie vidéo.
+Vous n'a pas besoin de \"faire télécharger la vidéo à un autre site\" tels que font la plupart des sites de téléchargement en ligne. 
+Veuillez vous assurer que vous avez déjà téléchargé youtube-dl avant d'utiliser ce script.
+Prends les URLs (de vidéos YouTube) de votre fichier \"url\", et télécharge les audios .mp3 corespondants.
+
+Options:
+	-h : affiche cette aide.
+	-o : spécifie le répertoire dans lequel seront enregistrés les audios.
+
+Futures options:
+	TODO -i : spécifie le répertoire où se trouve la liste d'URLs.
+	TODO -n : permet de choisir un autre nom que \"url\" pour le fichier contenant les URLs.
+	TODO -f : permet de choisir un format éventuellement autre que .mp3 .
+
+Exemples d'utilisation:
+----> basique:
+	./youtube-dl-audio.sh
+----> aide:
+	./youtube-dl-audio.sh -h
+----> enregistrer les audios dans \"Musiques\"
+	./youtube-dl-audio.sh -o ~/Musiques
+----> complet (depuis ~/Documents/url_list.txt vers ~/Musiques au format audio aac) :
+	./youtube-dl-audio.sh -o ~/Musiques -i ~/Documents -n url_list.txt -f aac
+----> si ces options ne vous suffisent pas:
+	Utilisez directement youtube-dl ou un équivalent, ou faites votre propre script.
+
+"
+	exit 0
+
+# -o specifie l'endroit ou l'on envoie les audios
+elif  [[ $1 = "-o" ]]; then
     cd $2 ||  
     {
     	echo "ERREUR: l'option -o necessite un chemin vers un dossier existant"
@@ -26,8 +61,8 @@ fi
 echo "telechargement d'une liste d'audios..."
 for var1 in `cat $dossier/$nom | grep '^[a-zA-Z]'`;do
 	echo "URL: $var1"
-	var2=$(youtube-dl --no-playlist -F $var1 | grep "audio only" | sed 's/ .*//' | sed -n "1 p")
-	youtube-dl  --no-playlist -x -f $var2 --audio-format mp3 $var1
+#	var2=$(youtube-dl --no-playlist -F $var1 | grep "audio only" | sed 's/ .*//' | sed -n "1 p")
+#	youtube-dl  --no-playlist -x -f $var2 --audio-format mp3 $var1
 	echo "fait"
 done
 
@@ -35,8 +70,8 @@ done
 echo "telechargement de playlists"
 for var1 in `cat $dossier/$nom | grep '^\+' | sed 's/^\+//'`;do
 	echo "URL: $var1"
-	var2=$(youtube-dl --yes-playlist -F $var1 | grep "audio only" | sed 's/ .*//' | sed -n "1 p")
-	youtube-dl  --yes-playlist -x -f $var2 --audio-format mp3 $var1
+#	var2=$(youtube-dl --yes-playlist -F $var1 | grep "audio only" | sed 's/ .*//' | sed -n "1 p")
+#	youtube-dl  --yes-playlist -x -f $var2 --audio-format mp3 $var1
 	echo "fait"
 done
 echo "telechargement termine"
