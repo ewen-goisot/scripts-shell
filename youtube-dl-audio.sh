39f830be3d6d14f433d82397de786cb6ff325dbe
ewen-goisot <ewengoisot@laposte.net>
2018-06-30 11:55:55 +0200

    silencieux, format par défaut
    ajout d'un mode silencieux permettant de ne pas avoir les messages de youtube-dl dans le terminal (voire de masquer les erreurs),
    ajout d'un mode ! pour télécharger l'audio sans convertir (recommandé sur ordinateur)
    ajout d'options libres du format youtube-dl avec -u -uv -up

--- youtube-dl-audio.sh
+++ youtube-dl-audio.sh
@@ -1,3 +1,4 @@
+#!/bin/bash
 # fonctionne sur GNU/Linux (testé), fonctionne probablement sur OS X (non testé)
 # si vous constatez une erreur ou voulez suggerer une modification, contactez moi à ewengoisot@laposte.net
 
@@ -16,6 +17,13 @@ dossier=$(pwd)
 nom="url.txt"
 format="mp3"
 actualise=""
+silence="&1"
+silence_erreur="&2"
+nom_audio="-o fichier_audio"
+num_audio="001"
+univ=""
+univ_video=""
+univ_playlist=""
 
 # -h affiche un message d'aide.
 aide () {
@@ -38,16 +46,27 @@ Options:
 	-n : permet de choisir un autre nom que $nom pour le fichier contenant les URLs.
 	-f : permet de choisir un format éventuellement autre que .mp3 .
 	-k : active l'actualisation: les URLs seront envoyées vers un fichier $nom.old.
+	-s : silencieux: n'affiche que les messages d'erreurs de youtube-dl, masque les autres.
+	-S : silencieux+: n'affiche ni les messages standards ni les messages d'erreurs de youtube-dl.
+	-u : rajoute n'importe quelle option de youtube-dl à chaque téléchargement
+	-uv : universel pour les vidéos seules uniquement
+	-up : universel pour les playlist uniquement
+	TODO:
+	-c : nomme les fichiers 001 002 003...
+	-c1 : comme -c, préciser le premier indice.
+	-cn : nomme les fichiers $nom_audio\_001 $nom_audio\_002 ...
+	-cn1 : -cn et -c1, précisez d'abord l'indice
 
 Futures options:
-	TODO : (s) sous-titres
-	TODO : (m) masquer les messages de youtube-dl
+	TODO : (t) sous-titres
 	TODO : moifications légères de l'aide selon les options précédemment sélectionnées (partiellement fait)
 
 Dans le fichier $nom:
 si le premier caractère d'une ligne est,
 	(pas de caractère special) : enregistre la vidéo seule (option --no-playlist dans youtube-dl).
+	! : garde le format par défaut (webm)
 	+ : télécharge la playlist correspondante lorsque cela a un sens, la vidéo seule sinon (option --yes-playlist)
+	!+ : playlist, garde le format webm
 	# : ne tient pas compte de cette ligne (la traite comme un commentaire)
 
 
@@ -59,7 +78,7 @@ Exemples d'utilisation:
 ----> enregistrer les audios dans \"Musiques\"
 	$thisthis -o ~/Musiques
 ----> complet (depuis ~/Documents/$nom vers ~/Musiques/ma_playlist_5 au format audio aac et actualiser) :
-	$thisthis https://www.youtube.com/watch?v=V2vSq0YoWjA.sh -O ~/Musiques/ma_playlist_5 -k -i ~/Documents -n $nom -f aac
+	$thisthis https://www.youtube.com/watch?v=V2vSq0YoWjA.sh -O ~/Musiques/ma_playlist_5 -k -i ~/Documents -n $nom -f aac -s
 ----> si ces options ne vous suffisent pas:
 	Utilisez directement youtube-dl ou un équivalent, ou faites votre propre script.
 
@@ -105,7 +124,29 @@ actualise () {
 	echo "les URLs seront transférées vers un fichier *.old"
 	actualise="methode_1"
 }
-
+silence () {
+	echo "les messages de youtube-dl ne seront pas affichés, SAUF en cas d'erreur"
+	echo "utilisez l'option -S pour masquer aussi les messages d'erreurs"
+	silence="/dev/null"
+}
+silence_tout () {
+	echo "les messages et messages d'erreurs de youtube-dl ne seront pas affichés"
+	echo "utilisez l'option -s pour masquer seulement les messages standards"
+	silence="/dev/null"
+	silence_erreur="/dev/null"
+}
+universel () {
+	echo "tous les telechargements sont affectes par l'option $1"
+	univ="$1"
+}
+universel () {
+	echo "tous les telechargements de VIDEOS SEULES sont affectes par l'option $1"
+	univ_video="$1"
+}
+universel () {
+	echo "tous les telechargements de PLAYLISTS sont affectes par l'option $1"
+	univ_playlist="$1"
+}
 
 # lecture des options sélectionées
 # vous avez le droit d'être "stupide" et de répéter plusieurs fois certaines options, dans ce cas, snerr, on tient compte du dernier changement.
@@ -126,9 +167,27 @@ while [[ -n $1 ]] ; do
 			shift 2;;
 		-k) actualise;
 			shift;;
+		-s) silence;
+			shift;;
+		-S) silence_tout;
+			shift;;
+		-u) universel $2;
+			shift 2;;
+		-uv) universel_video $2;
+			shift 2;;
+		-up) universel_playlist $2;
+			shift 2;;
 		*) echo "ERREUR: L'option $1 n'est pas reconnue."; exit 1; break;;
 	esac
 done
+
+#echo "récapitulatif:
+#répertoire du script: $initial
+#répertoire des url: $dossier
+#fichier des url: $dossier/$nom
+#format: $format
+#répertoire d'enregistrement des videos:"
+#pwd
 
 # vérifications -i / -n
 cat $dossier/$nom > /dev/null ||
@@ -138,20 +197,42 @@ cat $dossier/$nom > /dev/null ||
 }
 
 # videos seules
+echo "telechargement d'une liste d'audios SANS FORMATAGE..."
+for var1 in `cat $dossier/$nom | grep '^\![a-zA-Z]' | sed 's/^\!//'`;do
+	echo "URL: $var1"
+	var2=$(youtube-dl --no-playlist -F $var1 | grep "audio only" | sed 's/ .*//' | sed -n "1 p")
+	echo "SHELL: youtube-dl  --no-playlist -x -f $var2 \$URL $univ $univ_video"
+	youtube-dl  --no-playlist -x -f $var2 $var1 $univ $univ_video > $silence 2> $silence_erreur
+	echo "fait"
+done
+
 echo "telechargement d'une liste d'audios..."
 for var1 in `cat $dossier/$nom | grep '^[a-zA-Z]'`;do
 	echo "URL: $var1"
 	var2=$(youtube-dl --no-playlist -F $var1 | grep "audio only" | sed 's/ .*//' | sed -n "1 p")
-	youtube-dl  --no-playlist -x -f $var2 --audio-format $format $var1
+	echo "SHELL: youtube-dl  --no-playlist -x -f $var2 --audio-format $format \$URL $univ $univ_video"
+	youtube-dl  --no-playlist -x -f $var2 --audio-format $format $var1 $univ $univ_video > $silence 2> $silence_erreur
 	echo "fait"
 done
+
+# playlists NON FORMATEES
+echo "telechargement de playlists SANS FORMATAGE"
+for var1 in `cat $dossier/$nom | grep '^\+' | sed 's/^\+//'`;do
+	echo "URL: $var1"
+	var2=$(youtube-dl --yes-playlist -F $var1 | grep "audio only" | sed 's/ .*//' | sed -n "1 p")
+	echo "SHELL: youtube-dl  --yes-playlist -x -f $var2 \$URL $univ $univ_video"
+	youtube-dl  --yes-playlist -x -f $var2 $var1 $univ $univ_playlist > $silence 2> $silence_erreur
+	echo "fait"
+done
+echo "telechargement termine"
 
 # playlists
 echo "telechargement de playlists"
 for var1 in `cat $dossier/$nom | grep '^\+' | sed 's/^\+//'`;do
 	echo "URL: $var1"
 	var2=$(youtube-dl --yes-playlist -F $var1 | grep "audio only" | sed 's/ .*//' | sed -n "1 p")
-	youtube-dl  --yes-playlist -x -f $var2 --audio-format $format $var1
+	echo "SHELL: youtube-dl  --yes-playlist -x -f $var2 --audio-format $format \$URL $univ $univ_video"
+	youtube-dl  --yes-playlist -x -f $var2 --audio-format $format $var1 $univ $univ_playlist > $silence 2> $silence_erreur
 	echo "fait"
 done
 echo "telechargement termine"
